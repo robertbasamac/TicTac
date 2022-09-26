8fe8666b61736cbde4ee151e2636ba446e9c8422
Robert Basamac <rbasamac@gmail.com>
2022-09-26 14:27:57 +0300

    Initial Commit

---
+++ TicTac/TicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacApp.swift
+//  TicTac
+//
+//  Created by Robert Basamac on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
